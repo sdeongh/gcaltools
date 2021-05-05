559349a6411fbbcd1c2fabc94458b9be4fb7596c
Steve De Jongh <sdejongh@he2b.be>
2021-05-05 18:16:01 +0200

    Restructuring files and folder

--- gcaltools/config.py
+++ gcaltools/config.py
@@ -6,8 +6,6 @@ __VERSION = "0.5.1"
 # NEVER CHANGE THIS
 SCOPES = ['https://www.googleapis.com/auth/calendar.events', 'https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.addons.execute']
 DATE_FORMAT = "%Y-%m-%d"
-#DATETIME_FORMAT = "%Y-%m-%d %H:%M"
-#GCAL_DATE_FORMAT = "%Y-%m-%dT00:00:00+00:00"
 USER_PREFERENCES_FILE = '.gcaltools'
 COLORS = {
     'lavender': 1,
